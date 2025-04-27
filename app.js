592991401119064c4f98298630e40ec05d226c73
husicmirza <mirza.huc@gmail.com>
2025-04-27 18:22:13 +0300

    Integrate MongoDB connection and update environment variables for database URI.

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-
+import connectToDatabase from "./database/mongodb.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+  await connectToDatabase();
 });
 
 app.use("/api/v1/auth", authRouter);
-
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
