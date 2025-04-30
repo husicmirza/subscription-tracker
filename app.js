78749f87995b5dd8930576ed1048847dc603c97a
husicmirza <mirza.huc@gmail.com>
2025-04-30 12:34:17 +0300

    Add error handling middleware and update API response message

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to the API!");
 });
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
   await connectToDatabase();
 });
-
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export default app;
